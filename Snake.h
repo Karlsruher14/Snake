58dc475a3eb1b71af555592e4752c7b4d42a7175
Karlsruher14 <https://Karlsruher14@github.com>
2025-11-04 17:14:16 +0100

    class Snake.h added

---
+++ Snake.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include "Vec2.h"
+
+class Snake
+{
+    private:
+        std:::vector<vec2i> body;
+        vec2i direction;
+        bool glow;
+    
+    public:
+        Snake();
+        void move();
+        void move();
+        void setDirection(const vec2i& dir);
+        void growNext();
+        bool checkCollision() const;
+        const std::vector<vec2i>& getBody() const;
+};
