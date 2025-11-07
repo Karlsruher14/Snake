c106a64cf1c84205c13a12fef3b3cd0efa4fdc23
Karlsruher14 <https://Karlsruher14@github.com>
2025-11-07 15:55:36 +0100

    arrow control

--- processInput.cpp
+++ processInput.cpp
@@ -7,12 +7,12 @@ void processInput(GLFWwindow* window, Snake& snake)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
         snake.setDirection({0.0f, 1.0f});
-    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
         snake.setDirection({0.0f, -1.0f});
-    else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
         snake.setDirection({-1.0f, 0.0f});
-    else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
         snake.setDirection({1.0f, 0.0f});
 }
