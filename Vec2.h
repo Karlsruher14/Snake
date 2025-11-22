e18d318fd39ff93f843ff768bf99f0e089d5697c
Karlsruher14 <https://Karlsruher14@github.com>
2025-11-22 12:50:37 +0100

    New vec3 and vec4 structures have been added to Vec2.h

--- Vec2.h
+++ Vec2.h
@@ -16,3 +16,33 @@ struct vec2
     bool operator==(const vec2& other) const { return x == other.x && y == other.y; }
     bool operator!=(const vec2& other) const { return !(*this == other); }
 };
+
+struct vec3
+{
+    float x, y, z;
+
+    vec3(float value = 0.0f) : x(value), y(value), z(value) {}
+    vec3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+
+    vec3 operator+(const vec3& other) const { return vec3(x + other.x, y + other.y, z + other.z); }
+    vec3 operator-(const vec3& other) const { return vec3(x - other.x, y - other.y, z - other.z); }
+    vec3 operator*(const vec3& other) const { return vec3(x * other.x, y * other.y, z * other.z); }
+    vec3 operator/(const vec3& other) const { return vec3(x / other.x, y / other.y, z / other.z); }
+    bool operator==(const vec3& other) const { return x == other.x && y == other.y && z == other.z; }
+    bool operator!=(const vec3& other) const { return !(*this == other); }
+};
+
+struct vec4
+{
+    float x, y, z, w;
+
+    vec4(float value = 0.0f) : x(value), y(value), z(value), w(value) {}
+    vec4(float _x, float _y, float _z, float _w) : x(_x), y(_y), z(_z), w(_w) {}
+
+    vec4 operator+(const vec4& other) const { return vec4(x + other.x, y + other.y, z + other.z, w + other.w); }
+    vec4 operator-(const vec4& other) const { return vec4(x - other.x, y - other.y, z - other.z, w - other.w); }
+    vec4 operator*(const vec4& other) const { return vec4(x * other.x, y * other.y, z * other.z, w * other.w); }
+    vec4 operator/(const vec4& other) const { return vec4(x / other.x, y / other.y, z / other.z, w / other.w); }
+    bool operator==(const vec4& other) const { return x == other.x && y == other.y && z == other.z && w == other.w; }
+    bool operator!=(const vec4& other) const { return !(*this == other); }
+};
